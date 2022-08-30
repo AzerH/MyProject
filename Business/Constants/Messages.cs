5e0d2de7a6337dc27728931ea7643a7b63dd6dde
AzerH <30517450+AzerH@users.noreply.github.com>
2022-08-30 09:48:55 +0400

    30.08

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalit = "Product's name is invalit";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductListed = "Products are listed";
+    }
+}
